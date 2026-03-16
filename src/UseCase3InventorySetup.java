7110930bf39c1cd79d3cdcf26edb54fab69ca2b6
Manvi136 <manviagarwal25082006@gmail.com>
2026-03-16 09:17:51 +0530

    Use Case 3: Centralized Room Inventory Management

---
+++ src/UseCase3InventorySetup.java
@@ -0,0 +1,139 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * MAIN CLASS UseCase3InventorySetup
+ *
+ * Use Case 3: Centralized Room Inventory Management
+ *
+ * @version 3.1
+ */
+public class UseCase3InventorySetup {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Inventory Status");
+
+        // Create room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Initialize centralized inventory
+        RoomInventory inventory = new RoomInventory();
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        // Single Room
+        System.out.println("\nSingle Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Single"));
+
+        // Double Room
+        System.out.println("\nDouble Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Double"));
+
+        // Suite Room
+        System.out.println("\nSuite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Suite"));
+    }
+}
+
+/**
+ * CLASS RoomInventory
+ *
+ * Use Case 3: Centralized Room Inventory Management
+ *
+ * @version 3.1
+ */
+class RoomInventory {
+
+    // Stores available room count for each room type
+    // Key = Room Type , Value = Available Count
+    private Map<String, Integer> roomAvailability;
+
+    // Constructor
+    public RoomInventory() {
+        roomAvailability = new HashMap<>();
+        initializeInventory();
+    }
+
+    // Initialize default availability
+    private void initializeInventory() {
+        roomAvailability.put("Single", 5);
+        roomAvailability.put("Double", 3);
+        roomAvailability.put("Suite", 2);
+    }
+
+    // Return availability map
+    public Map<String, Integer> getRoomAvailability() {
+        return roomAvailability;
+    }
+
+    // Update availability
+    public void updateAvailability(String roomType, int count) {
+        roomAvailability.put(roomType, count);
+    }
+}
+
+/**
+ * ABSTRACT CLASS Room
+ *
+ * @version 3.0
+ */
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+/**
+ * CLASS SingleRoom
+ *
+ * @version 3.0
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+/**
+ * CLASS DoubleRoom
+ *
+ * @version 3.0
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+/**
+ * CLASS SuiteRoom
+ *
+ * @version 3.0
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
